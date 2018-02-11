0f060244a4ba72708fb34d3281fcf0ccfd0b46ed
NeoTime <riglerjeff@gmail.com>
2018-02-11 09:02:09 -0800

    Timing class
    add Timer and Timeout

---
+++ Ch03/03_04/start/timers.js
@@ -0,0 +1,21 @@
+var waitTime = 3000;
+var currentTime = 0;
+var waitInterval = 500;
+
+console.log("Wait for it");
+
+var interval = setInterval(function() {
+
+  currentTime += waitInterval;
+
+  console.log(`waiting ${currentTime/1000} seconds...`);
+
+}, waitInterval);
+
+
+setTimeout(function (){
+
+clearInterval(interval);
+  console.log("done");
+
+}, waitTime);
